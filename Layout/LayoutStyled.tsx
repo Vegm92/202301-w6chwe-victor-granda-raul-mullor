d92e8e938b29f5ca9d9bec6f79a1938a0672f558
RaulMullor <mullor92@gmail.com>
2023-02-19 12:30:20 +0100

    Add LayoutStyles

---
+++ Layout/LayoutStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const LayoutStyled = styled.div`
+  .main-header {
+    &__title {
+      font-family: monospace;
+      font-weight: 800;
+      font-size: 25px;
+    }
+  }
+`;
+
+export default LayoutStyled;
