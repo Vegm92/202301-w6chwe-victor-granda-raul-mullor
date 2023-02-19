2eeb02f5702d4d1d205519dc5155549a10d5a33b
RaulMullor <mullor92@gmail.com>
2023-02-19 12:38:27 +0100

    Create LayoutComponent

---
+++ Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LayoutStyled from "./LayoutStyled";
+
+const Layout = (): JSX.Element => {
+  return (
+    <LayoutStyled>
+      <header className="main-header">
+        <h1 className="main-header__title">Robots</h1>
+      </header>
+    </LayoutStyled>
+  );
+};
+
+export default Layout;
